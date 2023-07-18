b0659c2e587b7021444c594cad034556afdf2daa
Sirhc47 <125524491+Sirhc47@users.noreply.github.com>
2023-07-18 21:16:41 +1000

    Added code for motors
    rough code that needs to be tested and refined

--- Utilities.py
+++ Utilities.py
@@ -199,27 +199,77 @@ colourA = ColourSensor(1) #Right sensor
 colourB = ColourSensor(2) #Left sensor
 colourC = ColourSensor(3) #Front sensor
 distance = DistanceSensor(4) #Distance sensor
+
+# Define the GPIO pins for each motor (replace these with the actual GPIO pins you are using)
+MOTOR1_FORWARD_PIN = 17
+MOTOR1_BACKWARD_PIN = 18
+MOTOR1_PWM_PIN = 22
+
+MOTOR2_FORWARD_PIN = 23
+MOTOR2_BACKWARD_PIN = 24
+MOTOR2_PWM_PIN = 25
+
+MOTOR3_FORWARD_PIN = 5
+MOTOR3_BACKWARD_PIN = 6
+MOTOR3_PWM_PIN = 13
+
+MOTOR4_FORWARD_PIN = 19
+MOTOR4_BACKWARD_PIN = 26
+MOTOR4_PWM_PIN = 12
+
+# Initialize the four motors
+motor1 = Motor(MOTOR1_FORWARD_PIN, MOTOR1_BACKWARD_PIN, MOTOR1_PWM_PIN) #front left
+motor2 = Motor(MOTOR2_FORWARD_PIN, MOTOR2_BACKWARD_PIN, MOTOR2_PWM_PIN) #front right
+motor3 = Motor(MOTOR3_FORWARD_PIN, MOTOR3_BACKWARD_PIN, MOTOR3_PWM_PIN) #back left
+motor4 = Motor(MOTOR4_FORWARD_PIN, MOTOR4_BACKWARD_PIN, MOTOR4_PWM_PIN) #back right
+
+
+def turnRight(speed):
+        motor2.setSpeed(-speed);
+        motor4.setSpeed(-speed);
+        motor1.setSpeed(speed);
+        motor3.setSpeed(speed);
+def turnLeft(speed):
+    turnRight(-speed)
 while True: 
     if colourA.getHue() > 65 and colourA.getHue() < 75:
         # If right sensor touches black turn right 15 degrees
-         pass
+        turnRight(0.1)
+        pass
     if colourB.getHue() > 65 and colourA.getHue() < 75:
         # If left sensor touches black turn left 15 degrees
-         pass
-     if colourA.getHue() > 90 and colourA.getHue() < 100:
+        turnLeft(0.1)   
+        pass
+    if colourA.getHue() > 90 and colourA.getHue() < 100:
         #Move the right at 30 degrees until right sensor touches black
-         pass
+        while not(colourB.getHue() > 65 and colourB.getHue() < 75):
+            turnRight(0.1)
+        pass
     if colourB.getHue() > 90 and colourB.getHue() < 100:
         #Move to the left at 30 degrees until left sensor touches black
-         pass
+        while not(colourA.getHue() > 65 and colourA.getHue() < 75):
+            turnLeft(0.1)
+        pass
     if distance.getDistance() < 100:
         #Turn 90 degrees right
+        turnRight(0.1)
+        time.sleep(0.3)
         #Move at 30 degrees left for _ seconds
+        turnLeft(0.1)
         #When right sensor touches black, turn right until it is no longer touching black
-         pass
-     if colourA.getSaturation() > 2:
+        if colourB.getHue() > 65 and colourA.getHue() < 75:
+            while colourB.getHue() > 65 and colourA.getHue() < 75:
+                turnRight()
+        pass
+    if colourA.getSaturation() > 2:
         #Turn right 45 degrees
+        turnRight(0.1)
+        time.sleep(0.2)
         #move left slowly
-         pass
+        motor2.setSpeed(0.2)
+        motor4.setSpeed(0.2)
+        motor1.set(0.1)
+        motor3.setSpeed(0.1)
+        pass
 # Hue values for different colours at a distance of ≈ 2cm away: Black = 70, White = 79, Green = 92
     
